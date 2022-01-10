aec744725be1a0f4c52fbe03aca6cff2bef5ff3f
Rey Mercado <reymercado.usa@gmail.com>
2022-01-10 15:38:13 -0800

    WIP 6 tests passing

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -136,6 +136,15 @@ This function should use forEach to populate your grocery list based on the stor
 
 const createList = (availableItems) => {
   // Solution code here...
+  let resultsArr = [];
+
+  availableItems.forEach( item => {
+    if (item.available === true) {
+      resultsArr.push(item.name);
+    }
+  });
+
+  return resultsArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -198,7 +207,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   const inventory = [{ name: 'apples', available: true }, { name: 'pears', available: true }, { name: 'oranges', available: false }, { name: 'bananas', available: true }, { name: 'blueberries', available: false }];
 
   test('It should only add the available items to the list', () => {
